ee795da5b6131e0ebc5afe2a0f388986babccdcc
NicholasK <nicholask1@outlook.com>
2022-01-13 18:14:30 -0500

    Added home page route

---
+++ app.py
@@ -0,0 +1,21 @@
+"""Blogly application."""
+
+from flask import Flask, redirect, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "001HHUUDSKFUhhqyw56"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route("/")
+def home_page():
+
+    users = User.query.all()
+    return render_template("home_page.html", users=users)
